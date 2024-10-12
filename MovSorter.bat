0044f1df51cef69cb9c11471d9e50f05c64fde0f
AndreasNelson <nel02254@morris.umn.edu>
2024-10-12 03:12:29 -0500

    simplified movsorter

--- MovSorter.bat
+++ MovSorter.bat
@@ -1,27 +1,19 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Loop through all files in the current directory
 for %%f in (*.*) do (
-    REM Skip .bat and .cmd files
-    if /I not "%%~xf"==".bat" (
-        if /I not "%%~xf"==".cmd" (
-            REM Get the base name of the file (without extension)
-            set "filename=%%~nf"
-            
-            REM Remove additional extensions (e.g., .en.srt -> .srt)
-            for /f "tokens=1 delims=." %%a in ("!filename!") do (
-                set "basename=%%a"
-            )
-            
-            REM Create a directory with the base name if it doesn't exist
-            if not exist "!basename!" (
-                mkdir "!basename!"
-            )
-            
-            REM Move the file into the corresponding directory
-            move "%%f" "!basename!\"
+    if /I "%%~xf" neq ".bat" if /I "%%~xf" neq ".cmd" ( 
+        set "filename=%%~nf"
+
+        for /f "tokens=1 delims=." %%a in ("!filename!") do (
+            set "basename=%%a"
         )
+
+        if not exist "!basename!" (
+            mkdir "!basename!"
+        )
+
+        move "%%f" "!basename!\"
     )
 )
 
