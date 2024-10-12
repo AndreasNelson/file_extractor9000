c4212ec4f93f21ebeec1888e44ddff8b6078f3fa
AndreasNelson <nel02254@morris.umn.edu>
2024-10-12 03:16:20 -0500

    tried a chatgpt fix, idk man it's like 3 am

--- MovSorter.bat
+++ MovSorter.bat
@@ -1,20 +1,20 @@
-@echo off
-setlocal enabledelayedexpansion
-
 for %%f in (*.*) do (
+    echo Processing file: %%f
     if /I "%%~xf" neq ".bat" if /I "%%~xf" neq ".cmd" ( 
         set "filename=%%~nf"
+        echo Filename: !filename!
 
         for /f "tokens=1 delims=." %%a in ("!filename!") do (
             set "basename=%%a"
+            echo Basename: !basename!
         )
 
         if not exist "!basename!" (
+            echo Creating directory: !basename!
             mkdir "!basename!"
         )
 
-        move "%%f" "!basename!\"
+        echo Moving %%f to directory !basename!
+        move "%%f" "!basename!\" || echo Failed to move %%f
     )
 )
-
-endlocal
