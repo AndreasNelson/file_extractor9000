2d4ed0be68ad2af0cb2a33e6ad7ae641069e6511
AndreasNelson <nel02254@morris.umn.edu>
2024-10-12 01:41:05 -0500

    added bat and cmd file name extension to the searching step

--- MovSorter.bat
+++ MovSorter.bat
@@ -3,16 +3,19 @@ setlocal enabledelayedexpansion
 
 REM Loop through all files in the current directory
 for %%f in (*.*) do (
-    REM Get the base name of the file (without extension)
-    set "filename=%%~nf"
-    
-    REM Create a directory with the base name if it doesn't exist
-    if not exist "!filename!" (
-        mkdir "!filename!"
+    REM Skip .bat and .cmd files
+    if /I not "%%~xf"==".bat" if /I not "%%~xf"==".cmd" (
+        REM Get the base name of the file (without extension)
+        set "filename=%%~nf"
+        
+        REM Create a directory with the base name if it doesn't exist
+        if not exist "!filename!" (
+            mkdir "!filename!"
+        )
+        
+        REM Move the file into the corresponding directory
+        move "%%f" "!filename!\"
     )
-    
-    REM Move the file into the corresponding directory
-    move "%%f" "!filename!\"
 )
 
 endlocal
